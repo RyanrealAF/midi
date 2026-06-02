2d7fc404b8f55acf5525db384f32faf1a316113e
purarecoveryryan <purarecoveryryan@gmail.com>
2026-06-02 07:37:54 +0000

    kdkd

---
+++ .idx/dev.nix
@@ -0,0 +1,55 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "dbaeumer.vscode-eslint",
+      "esbenp.prettier-vscode",
+      "vscodevim.vim",
+      "github.copilot",
+      "ms-azuretools.vscode-docker",
+      "mutantdino.resourcemonitor",
+      "redhat.vscode-yaml",
+      "swellaby.vscode-rust-test-adapter"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npx", "http-server", "-p", "$PORT"];
+          manager = "web";
+          env = {
+            PORT = "$PORT";
+          };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
